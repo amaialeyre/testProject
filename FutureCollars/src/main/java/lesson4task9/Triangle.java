62f7610c779c60ed64c715fca060b0140fa917cd
Anna Maras <aniamaras@wp.pl>
2023-01-24 20:05:11 +0100

    Created class Triangle

---
+++ FutureCollars/src/main/java/lesson4task9/Triangle.java
@@ -0,0 +1,32 @@
+package lesson4task9;
+
+public class Triangle {
+    private int width;
+    private int height;
+
+    public Triangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle width: " + width + ", triangle height: " + height;
+    }
+}
