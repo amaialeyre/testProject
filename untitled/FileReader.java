5fb166f89ae30e00920131bf0f5cd2b9fbb38187
Anna Maras <aniamaras@wp.pl>
2023-01-26 06:25:19 +0100

    data.txt read

---
+++ untitled/FileReader.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileReader {
+    public static void readFile(String filePath) {
+        try {
+            String text = Files.readString(Path.of(filePath));
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
