e7690136c0be63adaa61a26eda62e056a2a9d414
Anna Maras <aniamaras@wp.pl>
2023-01-27 17:13:25 +0100

    Created class PhoneBook

---
+++ FutureCollars/src/main/java/lesson10task1/PhoneBook.java
@@ -0,0 +1,45 @@
+package lesson10task1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Optional;
+
+public class PhoneBook {
+
+    private final HashMap<String, Record> bookMap;
+
+    public PhoneBook() {
+        bookMap = new HashMap<>();
+        final String DELIMITER = ", ";
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/book.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(DELIMITER);
+                Record record = new Record();
+                record.setFullName(values[0]);
+                record.setAddress(values[1]);
+                if (values.length == 3) {
+                    record.setPhoneNumber(Optional.of(values[2]));
+                } else {
+                    record.setPhoneNumber(Optional.empty());
+                }
+                bookMap.put(values[0], record);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printBook() {
+        bookMap.forEach((k, v) -> System.out.println((k + ":" + v)));
+    }
+
+    public Record getRecord(String fullName) throws Exception {
+        if (bookMap.containsKey(fullName)) {
+            return bookMap.get(fullName);
+        }
+        throw new Exception("Key not found");
+    }
+}
