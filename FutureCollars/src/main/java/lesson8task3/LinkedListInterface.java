548f9b60701d33e819fc8ce1c7665eed2201efe2
Anna Maras <aniamaras@wp.pl>
2023-01-27 08:31:20 +0100

    Created LinkedList

---
+++ FutureCollars/src/main/java/lesson8task3/LinkedListInterface.java
@@ -0,0 +1,12 @@
+package lesson8task3;
+
+public interface LinkedListInterface {
+
+    int size();
+
+    void add(String element);
+
+    void insert(int index, String element);
+
+    void remove(int index);
+}
