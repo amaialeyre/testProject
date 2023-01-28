99d8c4036b366d08ca31855678dfef132b33640a
Anna Maras <aniamaras@wp.pl>
2023-01-28 07:47:21 +0100

    Created class Letters

---
+++ FutureCollars/src/main/java/lesson11task5/Letters.java
@@ -0,0 +1,25 @@
+package lesson11task5;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+
+public class Letters {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Dzisiaj", "jest", "sobota");
+
+        HashSet<String> characters = distinctLettersFromWords(words);
+        System.out.println(characters);
+    }
+
+    public static HashSet<String> distinctLettersFromWords(List<String> words) {
+        HashSet<String> letters = new HashSet<>();
+        for (String word : words) {
+            word.codePoints().mapToObj(character -> String.valueOf((char) character))
+                    .distinct()
+                    .forEach(letter -> letters.add(letter.toUpperCase(Locale.ROOT)));
+        }
+        return letters;
+    }
+}
