8cca3029180a8d1b78844bb2e763accd9304c848
Anna Maras <aniamaras@wp.pl>
2023-01-28 07:24:10 +0100

    Created class Cinema

---
+++ FutureCollars/src/main/java/lesson11task3/CinemaHall.java
@@ -0,0 +1,32 @@
+package lesson11task3;
+
+public class CinemaHall {
+    private String name;
+    private HallType hallType;
+
+    public CinemaHall(String name, HallType hallType) {
+        this.name = name;
+        this.hallType = hallType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HallType getHallType() {
+        return hallType;
+    }
+
+    public void setHallType(HallType hallType) {
+        this.hallType = hallType;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ FutureCollars/src/main/java/lesson11task3/HallType.java
@@ -0,0 +1,18 @@
+package lesson11task3;
+
+public enum HallType {
+    DD("2D"),
+    DDD("3D"),
+    DDDD("4D");
+
+    private final String correctHallName;
+
+    HallType(String correctHallName) {
+        this.correctHallName = correctHallName;
+    }
+
+    @Override
+    public String toString() {
+        return correctHallName;
+    }
+}
