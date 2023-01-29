5696d36d8fd5c06473345b6700e729d945562a3e
Anna Maras <aniamaras@wp.pl>
2023-01-29 23:49:52 +0100

    Lesson7task6

---
+++ FutureCollars/src/main/java/lesson7task6/CurrencyExchangeService.java
@@ -0,0 +1,10 @@
+package lesson7task6;
+
+public class CurrencyExchangeService implements ExchangeService {
+    private double exchangeRate;
+
+    @Override
+    public double fetchExchangeRate(Currencies currencyForSale, Currencies currencyToBuy) {
+        return exchangeRate;
+    }
+}
