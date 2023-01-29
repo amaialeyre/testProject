2ce26ff425b32ee47a328cf76a4a339c1a5159df
Anna Maras <aniamaras@wp.pl>
2023-01-30 00:37:14 +0100

    Lesson7task7

---
+++ FutureCollars/src/main/java/lesson7task7/ErrorCode.java
@@ -0,0 +1,6 @@
+package lesson7task7;
+
+public class ErrorCode {
+    public static final int USER_INPUT_ERROR = -1;
+}
+
---
+++ FutureCollars/src/main/java/lesson7task7/RatingAssignment.java
@@ -0,0 +1,15 @@
+package lesson7task7;
+
+class RatingAssignment {
+    public static Rating getRating(double rating) {
+        if (rating < 1.2) {
+            return Rating.SMALL;
+        } else if (1.2 <= rating && rating < 2) {
+            return Rating.GOOD;
+        } else if (2 <= rating && rating < 3) {
+            return Rating.VERY_GOOD;
+        } else {
+            return Rating.EXCELLENT;
+        }
+    }
+}
