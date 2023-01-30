52372965a96e54b2919fc2695f4bd63e36f11ba3
Anna Maras <aniamaras@wp.pl>
2023-01-30 01:00:04 +0100

    Lesson7task8

---
+++ FutureCollars/src/main/java/lesson7task8/SalaryCalculator.java
@@ -0,0 +1,16 @@
+package lesson7task8;
+
+public class SalaryCalculator {
+    private BaseSalaryService baseSalaryService;
+    private SaturdaySalaryService saturdaySalaryService;
+
+    public SalaryCalculator(BaseSalaryService baseSalaryService, SaturdaySalaryService saturdaySalaryService) {
+        this.baseSalaryService = baseSalaryService;
+        this.saturdaySalaryService = saturdaySalaryService;
+    }
+
+    public double calculateSalary(String surname, double bonus, int numberOfSaturday) {
+
+        return baseSalaryService.fetchBaseSalary(surname) + bonus + (saturdaySalaryService.fetchSaturdaySalary(surname) * numberOfSaturday);
+    }
+}
---
+++ FutureCollars/src/main/java/lesson7task8/SaturdaySalaryService.java
@@ -0,0 +1,5 @@
+package lesson7task8;
+
+public interface SaturdaySalaryService {
+    double fetchSaturdaySalary(String surname);
+}
