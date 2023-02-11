2274dd2ac9f7aa43eefbd936bfe7f93d25ffb1df
Anna Maras <aniamaras@wp.pl>
2023-02-11 10:53:05 +0100

    Lesson11task5 corrected

--- FutureCollars/src/main/java/lesson11task5/TextUtils.java
+++ FutureCollars/src/main/java/lesson11task5/TextUtils.java
@@ -2,8 +2,6 @@ package lesson11task5;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class TextUtils {
     public static void main(String[] args) {
