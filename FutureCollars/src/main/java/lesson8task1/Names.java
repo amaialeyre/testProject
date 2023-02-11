551adcf506c27d7768ba6f55d527da37db197815
Anna Maras <aniamaras@wp.pl>
2023-02-11 10:30:46 +0100

    Lesson8task1 corrected

--- FutureCollars/src/main/java/lesson8task1/Names.java
+++ FutureCollars/src/main/java/lesson8task1/Names.java
@@ -5,38 +5,26 @@ import java.util.*;
 public class Names {
 
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Anna");
-        names.add("Katarzyna");
-        names.add("Tymon");
-        names.add("Tadeusz");
-        names.add("Anna");
-        names.add("Karolina");
-        names.add("Dorota");
-        names.add("Piotr");
-
-    List<String> uniqueNames = new ArrayList<>();
-        for (String name : names) {
-        if (!uniqueNames.contains(name)) {
-            uniqueNames.add(name);
-        }
+        List<String> names = Arrays.asList("Anna", "Katarzyna", "Tymon", "Tadeusz", "Anna", "Karolina", "Dorota",
+                "Piotr");
+        System.out.println("List without duplicates: " + getListWithoutDuplicates(names));
+        System.out.println("List reversed: " + getListInReverseOrder(names));
+        System.out.println("List after change: " + getListWithReplacedNames(names));
     }
-        System.out.println("List without duplicates: " + uniqueNames);
 
-    List<String> reverseNames = new ArrayList<>();
-        for (int i = names.size() - 1; i >= 0; i--) {
-        reverseNames.add(names.get(i));
+    private static List<String> getListWithoutDuplicates(List<String> names) {
+        return new ArrayList<>(new HashSet<>(names));
     }
-        System.out.println("Elements in reverse order: " + reverseNames);
 
-    String changeFrom = "Anna";
-    String changeTo = "Joanna";
-        for (int i = 0; i < names.size(); i++) {
-        if (names.get(i).equals(changeFrom)) {
-            names.set(i, changeTo);
-        }
+    private static List<String> getListInReverseOrder(List<String> names) {
+        List<String> namesCopied = new ArrayList<>(names);
+        Collections.reverse(namesCopied);
+        return namesCopied;
     }
-        System.out.println("Elements after change: " + changeFrom + "->" + changeTo + "");
-        System.out.println(names);
-}
+
+    private static List<String> getListWithReplacedNames(List<String> names) {
+        List<String> namesCopied = new ArrayList<>(names);
+        Collections.replaceAll(namesCopied, "Anna", "Joanna");
+        return namesCopied;
+    }
 }
